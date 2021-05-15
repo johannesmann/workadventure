2ed046e5fa71292387a5bf39b4e93ee105325722
johannesmann <69393131+johannesmann@users.noreply.github.com>
2021-05-15 11:47:13 +0200

    Create hanni copy.tsx

---
+++ hanni copy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="hanni copy" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="121" columns="11">
+ <image source="hanni copy.png" width="374" height="374"/>
+</tileset>
