b1a2b2c40a8840577872cc57f2f1ac121aeca2bb
johannesmann <69393131+johannesmann@users.noreply.github.com>
2021-05-15 11:47:20 +0200

    Create Hanni.tsx

---
+++ Hanni.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="hanni" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="121" columns="11">
+ <image source="hanni.png" width="374" height="374"/>
+</tileset>
