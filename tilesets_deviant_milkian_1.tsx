723aa5bc1417befcdecb436de35808369e656ea2
johannesmann <69393131+johannesmann@users.noreply.github.com>
2021-05-15 11:47:29 +0200

    Create tilesets_deviant_milkian_1.tsx

---
+++ tilesets_deviant_milkian_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="289" columns="17">
+ <image source="tilesets_deviant_milkian_1.png" width="544" height="544"/>
+</tileset>
